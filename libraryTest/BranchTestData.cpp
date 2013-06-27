bed0afb684372253e89746491999039684d14cc3
jlangr <jeff@langrsoft.com>
2013-06-26 22:58:03 -0600

    good set
    added sln

---
+++ libraryTest/BranchTestData.cpp
@@ -0,0 +1,6 @@
+#include "BranchTestData.h"
+
+Branch BranchTestData::EAST_BRANCH = Branch("1", "east");
+Branch BranchTestData::WEST_BRANCH = Branch("2", "west");
+Branch BranchTestData::ArbitraryBranch = Branch("3", "rockrimmon");
+
