aa7e586d8179efeef7dae689fc488ca814663f0e
jlangr <jeff@langrsoft.com>
2013-06-26 21:00:55 -0600

    temp
    fix

---
+++ libraryTest/BranchTestData.cpp
@@ -0,0 +1,6 @@
+#include "BranchTestData.h"
+
+const Branch BranchTestData::EAST_BRANCH = Branch("1", "east");
+const Branch BranchTestData::WEST_BRANCH = Branch("2", "west");
+const Branch BranchTestData::ArbitraryBranch = Branch("3", "rockrimmon");
+
--- libraryTest/BranchTestData.h
+++ libraryTest/BranchTestData.h
@@ -1,10 +1,12 @@
 #ifndef BRANCHTEST_H
 #define BRANCHTEST_H
 
-namespace BranchTestData {
-   Branch EAST_BRANCH("1", "east");
-   Branch WEST_BRANCH("2", "west");
-   Branch ArbitraryBranch("3", "rockrimmon");
+#include "Branch.h"
+
+class BranchTestData {
+   static const Branch EAST_BRANCH;
+   static const Branch WEST_BRANCH;
+   static const Branch ArbitraryBranch;
 };
 
 #endif
